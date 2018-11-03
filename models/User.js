d51e973c1c1717ea5681df9ee3ca49c0ddae3687
PaingSoe <paingsoeaung.93@gmail.com>
2018-11-03 16:57:50 +0630

    Payment with Stripe and client side react app Added

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-const {
-    Schema
-} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model("users", userSchema);
